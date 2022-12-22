1ea83ca3cfbf928b6146dad38b7222dcfd026379
Giordani da Silveira dos Santos <dani.santossilveira@hotmail.com>
2022-12-22 08:37:40 -0300

    listando meus anuncios

--- app/src/main/java/giordanisilveirasantos/exercise/olx/olxapp/activity/MeusAnunciosActivity.java
+++ app/src/main/java/giordanisilveirasantos/exercise/olx/olxapp/activity/MeusAnunciosActivity.java
@@ -4,7 +4,12 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
@@ -13,14 +18,27 @@ import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 import giordanisilveirasantos.exercise.olx.olxapp.R;
+import giordanisilveirasantos.exercise.olx.olxapp.adapter.AdapterAnuncios;
 import giordanisilveirasantos.exercise.olx.olxapp.databinding.ActivityMeusAnunciosBinding;
+import giordanisilveirasantos.exercise.olx.olxapp.helper.ConfiguracaoFirebase;
+import giordanisilveirasantos.exercise.olx.olxapp.model.Anuncio;
 
 public class MeusAnunciosActivity extends AppCompatActivity {
 
     private AppBarConfiguration appBarConfiguration;
     private ActivityMeusAnunciosBinding binding;
+    private RecyclerView recyclerAnuncios;
+    private List<Anuncio> anuncios = new ArrayList<>();
+    private AdapterAnuncios adapterAnuncios;
+    private DatabaseReference anuncioUsuarioRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +47,19 @@ public class MeusAnunciosActivity extends AppCompatActivity {
         binding = ActivityMeusAnunciosBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        //configurações iniciais
+        anuncioUsuarioRef = ConfiguracaoFirebase.getFirebase().child("meus_anuncios").child(ConfiguracaoFirebase.getIdUsuario());
+
+        inicializaComponentes();
+
         setSupportActionBar(binding.toolbar);
+
+        recyclerAnuncios.setLayoutManager(new LinearLayoutManager(this));
+        recyclerAnuncios.setHasFixedSize(true);
+        adapterAnuncios = new AdapterAnuncios(anuncios, this);
+        recyclerAnuncios.setAdapter(adapterAnuncios);
+
+        recuperarAnuncios();
 
         //NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_meus_anuncios);
         //appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
@@ -41,6 +71,29 @@ public class MeusAnunciosActivity extends AppCompatActivity {
                 startActivity(new Intent(getApplicationContext(), CadastrarAnuncioActivity.class));
             }
         });
+    }
+
+    private void recuperarAnuncios(){
+        anuncioUsuarioRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                anuncios.clear();
+                for(DataSnapshot ds : snapshot.getChildren()){
+                    anuncios.add(ds.getValue(Anuncio.class));
+                }
+                Collections.reverse(anuncios);
+                adapterAnuncios.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    public void inicializaComponentes(){
+        recyclerAnuncios = findViewById(R.id.recyclerAnuncios);
     }
 
 }
